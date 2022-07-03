f018b00c98159f503f61dedf5351f89376485e60
Anastasia Semenovich <81320487+Anastasiasemen@users.noreply.github.com>
2022-07-03 17:50:04 +0300

    Update Routes.js

--- counter/src/routes/Routes.js
+++ counter/src/routes/Routes.js
@@ -7,7 +7,7 @@ import HomePageContainer from "../Pages/HomePage/containers/HomePageContainer";
 
 const Routes = () => {
     return (
-        <Switch> /* компонент из react-dom библ */
+        <Switch> 
             <Route exact path={ROUTES.HOME_PAGE} component={HomePageContainer}/>
             <Route path={ROUTES.COUNTER_PAGE} component={CounterContainer}/>
         </Switch>
@@ -15,9 +15,3 @@ const Routes = () => {
 };
 
 export default Routes;
-/* Route- сам путь к нашему компоненту;
-   path - "вот по этому адресу";
-   component={} - что будет показываться по этому адресу;
-   exact - когда мы заворачиваем в Switch, идет поверхностное сравнение,
-   а exact говорит сравнивать дико строго, более точно сверять пкти роутеру
-*/
